dbb0ab860fa8d43b258ba5ab8b4df0cf9342300a
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-19 10:38:40 -0300

    Creating redux folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routing from './routes';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
- return (
-   <div>
-      <Routing/>
-   </div>
- ); 
+  return (
+    <div>
+      <Provider store={store}>
+        <Routing />
+      </Provider>
+    </div>
+  );
 }
---
+++ src/store/reducer/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import reserve from "./reserveReducer";
+
+export default combineReducers({reserve})
