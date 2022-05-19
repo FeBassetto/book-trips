9385919505b204d74d8b48f6552436657762c13c
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-19 10:26:54 -0300

    Finished Reserve page style

--- src/pages/Reserves/Reserves.jsx
+++ src/pages/Reserves/Reserves.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Reserves.css'
+import {MdDelete} from 'react-icons/md'
 
 export default function Reserves() {
- return (
-   <div>
-        <h1>Reserves</h1>
-   </div>
- );
+  return (
+    <div>
+      <h1 className='title'>Voce solicitou 1 reserva</h1>
+
+      <div className='reserves'>
+        <img src=' https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg' 
+        alt="Maceio" />
+        <strong>Viagem Maceio 7 Dias</strong>
+        <span>Quantidade: 2</span>
+        <button
+        type='button'
+        onClick={() => {}}
+        >
+          <MdDelete size={20} color='#191919' />
+        </button>
+      </div>
+
+      <footer>
+          <button type='button'>Solicitar Reservas</button>
+      </footer>
+    </div>
+  );
 }
