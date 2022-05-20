79bd6f3beaa29b223090815cde168b9016b98cd5
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-20 10:19:43 -0300

    Add to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,26 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from 'redux-saga'
+import { loadState, saveState } from "./localStorage";
 
 import reducers from './reducer';
 import Saga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware()
-
 const enhancer = applyMiddleware(sagaMiddleware)
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const localStoreState = loadState()
 
 const store = createStore(
     reducers,
+    localStoreState,
     composeEnhancers(enhancer)
 )
 
+
 sagaMiddleware.run(Saga)
+
+store.subscribe(() => {
+    saveState({reserve: store.getState().reserve})
+})
 
 export default store
